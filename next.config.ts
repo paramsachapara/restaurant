e23971bb0e8bc73495814eb78d00dc4378e168c2
paramsachapara <paramsachapara29@gmail.com>
2024-12-18 03:04:33 +0530

    chore added output in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Added this line to enable static export
   images: {
     domains: ["images.getbento.com"], // Add the domain here
   },
