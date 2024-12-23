b17e7f51d0aed69b67e2c7d7ba4e3c4bde03a1af
paramsachapara <paramsachapara29@gmail.com>
2024-12-23 20:31:25 +0530

    Add ico

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export", // Added this line to enable static export
+  // output: "export", // Added this line to enable static export
   images: {
     domains: ["images.getbento.com"], // Add the domain here
   },
